0a88097988728194e0929fa48939baa65106b8ba
Kevin Sanders <kevin@c2cschools.com>
2018-02-08 18:32:08 -0500

    Added missing scripts.

--- scripts/build-docs-local.sh
+++ scripts/build-docs-local.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Variables
-JSDOC_PATH="./node_modules/@lukechavers/jsdoc/node_modules/jsdoc/jsdoc.js"
+#JSDOC_PATH="./node_modules/@lukechavers/jsdoc/node_modules/jsdoc/jsdoc.js"
+JSDOC_PATH="./node_modules/jsdoc/jsdoc.js"
 CONFIG_PATH="./docs/config/jsdoc-html-local.json"
 DOC_ROOT_PATH="./docs/html"
 DOC_DEST_PATH="$DOC_ROOT_PATH/latest"
@@ -9,11 +10,11 @@ TEMPLATE_PATH="./node_modules/@lukechavers/jsdoc"
 TUTORIAL_PATH="./docs/tutorials"
 
 # Say Hello
-#echo " "
-#echo " > Clearing existing docs at '$DOC_ROOT_PATH'"
-#echo " "
+echo " "
+echo " > Clearing existing docs at '$DOC_ROOT_PATH'"
+echo " "
 
-#rm -rf "$DOC_ROOT_PATH"
+rm -rf "$DOC_ROOT_PATH"
 
 echo " "
 echo " > Building Docs at '$DOC_DEST_PATH'"
